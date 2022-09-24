3d405a03cece0e9fa09e712ffe57f4102c091000
Pavel Hubarenka <pavel-se47@mail.ru>
2022-09-25 02:08:55 +0300

    mod 8 web 15.4

--- js/menu.js
+++ js/menu.js
@@ -5,9 +5,9 @@
   menuBtnRef.addEventListener('click', () => {
     const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-    menuBtnRef.classList.toggle('button-menu__is-open');
+    menuBtnRef.classList.toggle('button-menu--is-open');
     menuBtnRef.setAttribute('aria-expanded', !expanded);
 
-    mobileMenuRef.classList.toggle('button-menu__content-is-open');
+    mobileMenuRef.classList.toggle('button-menu__content--is-open');
   });
 })();
